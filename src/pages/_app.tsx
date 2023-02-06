1ee7386c3e0ef383cfba1f897f4111adcb3772cd
Kai Takami <kaitakami@protonmail.com>
2023-02-06 22:48:07 +0900

    Add some basics classes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { api } from "../utils/api";
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return <div className="bg-gradient-to-b from-[#1f1f1f] to-[#15162c] text-white min-h-screen selection:bg-green-400 selection:text-blue-500  "><Component {...pageProps} /></div>;
 };
 
 export default api.withTRPC(MyApp);
